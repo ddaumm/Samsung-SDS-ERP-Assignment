13cc12f9782a5b730d3740d789212120ade91da5
ddaumm <dlaekdna4862@gmail.com>
2025-10-16 12:12:01 +0900

    agent.py의 파일 이름 변경. 메모리를 설정하여, 자동으로 multi-turns chat이 이루어질 수 있도록 하는 기능 구현.

---
+++ LangChain ReAct Agent/chat_agent.py
@@ -0,0 +1,36 @@
+from langchain_openai import ChatOpenAI
+from langchain import hub
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
+
+from config import OPENAI_API_KEY
+from tools import create_rag_tool, create_google_search_tool, create_website_opener_tool
+
+tools = [
+    create_rag_tool('detailed_graduation_rules.pdf', 'utf-8', 300, 100, 'detailed_graduation_rules_retriever', '서울대학교 산업공학과 졸업을 위한 졸업 요건에 대한 세부 사항을 물어보는 질문에 답할 때 사용'),
+    create_google_search_tool(),
+    create_website_opener_tool()
+]
+
+llm = ChatOpenAI(
+    model='gpt-4-turbo',
+    api_key=OPENAI_API_KEY,
+    temperature=0
+)
+
+prompt = hub.pull('hwchase17/react-chat')
+
+agent = create_react_agent(llm, tools, prompt)
+
+# ConversationBufferMemory vs ConversationSummaryMemory (추후, 결정하기)
+memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+
+agent_executor = AgentExecutor(
+    agent=agent, 
+    tools=tools,
+    memory=memory,
+    # ReAct 과정 출력 여부
+    verbose=True,
+    # 출력 형식 오류 자동 처리 및 복구 여부
+    handling_parsing_erros=True
+)
